3a1912025248439a543f07dcb98ec79d14817b79
Fernando Salamanca <lfernandosalamanca@gmail.com>
2021-11-28 19:52:09 +0000

    first commits

---
+++ src/todos.py
@@ -0,0 +1,18 @@
+todos = [
+    {
+        "done":True,
+        "label": "Test todo 1"
+    },
+    {
+        "done":False,
+        "label": "Test todo 2"
+    },
+    {
+        "done":True,
+        "label": "Test todo 3"
+    },
+    {
+        "done":False,
+        "label": "Test todo 4"
+    }
+    ]
